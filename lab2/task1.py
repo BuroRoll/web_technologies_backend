6cdf63edf817777bbb717011bed4ed8638cfe5d8
danilkonkov <buroroll@gmail.com>
2024-06-10 22:33:02 +0500

    Домашняя работа 2

--- lab2/task1.py
+++ lab2/task1.py
@@ -1,14 +1,24 @@
-if __name__ == '__main__':
-    try:
-        value = int(input("Введите число и я определю четное оно или нет: "))
-    except ValueError:
-        print('Введено не число')
-        exit(1)
-    if value % 2 == 0:
-        print(f'Число {value} четное')
-    else:
-        print(f'Число {value} нечетное')
+import boto3
 
 
-print('test')
-print('test2')
+
+class Storage:
+    def __init__(self, bucket_name):
+        self.bucket_name = bucket_name
+        self.s3 = boto3.client('s3',
+                               endpoint_url='storage.yandexcloud.net',
+                               region_name='u-central1-a',
+                               aws_access_key_id='YCAJEhWoQ1CM6BsfAlpT4SL3M',
+                               aws_secret_access_key='YCMJpHzBM_foD-Qa26G1RjWE47Dj3Mh9CtFFYEF2'
+                               )
+
+    def save_file(self, file_name):
+        try:
+            self.s3.upload_file('f' + file_name, self.bucket_name, file_name)
+        except Exception as e:
+
+            raise e
+
+        file_url = f'{self.s3.meta.endpoint_url}/{self.bucket_name}/{file_name}'
+
+        return file_url
